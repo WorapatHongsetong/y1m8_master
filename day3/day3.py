ec996cf82fc9de0fd6df7a7d47d4426eb97cf3da
QuantumTacoNinja <worapat1h@gmail.com>
2025-02-06 13:11:06 +0700

    finisehd day 3

--- day3/day3.py
+++ day3/day3.py
@@ -1,12 +1,15 @@
-def contraction_test(func, x_0, p=1e-10):
-    x = x_0
-    iterations = 1
-    while True:
+def contraction_test(func, start, end, max_iter=1000,  p=1e-10):
+    x = start
+    for i in range(max_iter):
         if abs(func(x) - x) <= p:
             return x
-        print(f"Iteration: {iterations}, Value: {x}")
-        x += p
-        iterations += 1
+        if i > 0 and abs(x_prev - x) <= p:
+            return x
+        print(f"Iteration: {i}, Value: {x}")
+
+        x_prev = x
+        x += (end - start)/max_iter
+
 
 def f(x):
     return x**2 -x -2
@@ -21,7 +24,7 @@ def dg_3(x):
     return -2/(x**2)
 
 def dg_4(x):
-    return -1/6 * (x**2 -x -2) + x
+    return -1/600 * (x**2 -x -2) + x
 
 # fixed_points = []
 # fixed_points.append(contraction_test(dg_1, -1/2, p=1e-4))
@@ -31,4 +34,4 @@ def dg_4(x):
 # fixed_points.append(contraction_test(dg_3, 2**0.5, p=1e-4))
 # print(fixed_points)
 
-print(contraction_test(dg_4, 1, p=1e-4))
+print(contraction_test(dg_4, 1, 3, 1000, p=1e-4))
